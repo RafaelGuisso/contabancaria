2bfbc158b39ad8808f9ea5f9b02408a3b9cd90d2
Rafael Guisso <rafaelguisso@gmail.com>
2024-02-26 08:17:52 -0300

    mandando controller e contas correte e poupanca

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,27 +1,101 @@
 package conta;
 
-import conta.model.Conta;
+import java.util.Scanner;
+
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
+import conta.util.Cores;
 
 public class Menu {
 
+	public static Scanner leia = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		
-	  	Conta c1 = new Conta(123456, 123, 1, "Maria dos Santos", 2500.0f);
-    	c1.visualiza(0);
-    
-    	Conta c2 = new Conta(123457, 123, 1, "João da Silva", 2000.0f);
-    	c2.visualiza(0);
-    	
-    	Conta c3 = new Conta(123458, 123, 1, "Maria Joaquina", 100000.0f );
-    	c3.visualiza(0);
 
-		if (c1.sacar(100))
-			System.out.println("Saque autorizado. O Saldo do cliente é de: "  + c1.getSaldo());
-		else
-			System.out.println("Saldo insuficiente. O Saldo do cliente é de: " + c1.getSaldo());
+		int opcao;
+        
+		// Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
 		
-		c1.depositar(1000);
-		System.out.println("Deposito realizado. O Saldo do cliente é de: " + c1.getSaldo());
+        // Teste da Classe Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
+		cp1.visualizar();
+        cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
+		
+		while (true) {
+			
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("                APLICATIVO DO BANCO                  ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Criar Conta                          ");
+			System.out.println("            2 - Listar todas as Contas               ");
+			System.out.println("            3 - Buscar Conta por Numero              ");
+			System.out.println("            4 - Atualizar Dados da Conta             ");
+			System.out.println("            5 - Apagar Conta                         ");
+			System.out.println("            6 - Sacar                                ");
+			System.out.println("            7 - Depositar                            ");
+			System.out.println("            8 - Transferir valores entre Contas      ");
+			System.out.println("            9 - Sair                                 ");
+			System.out.println("                                                     "); 
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println("                                                     ");
+			opcao = leia.nextInt();
+			
+			if(opcao == 9) {
+				System.out.println("\nEncerrando!");
+				leia.close();
+				System.exit(0);
+			}
+			
+			switch(opcao) {
+			case 1:
+				System.out.println("\n Criar Conta");
+				
+                 break;
+			case 2:
+				System.out.println("\n Listar todas as Contas");
+				
+                 break;
+			case 3:
+				System.out.println("\n Buscar Conta por número");
+				
+				break;
+			case 4:
+				System.out.println("\n Atualizar dados da Conta");
+				
+                 break;
+			case 5:
+				System.out.println("\n Apagar Conta");
+				
+                 break;
+			case 6:
+				System.out.println("\n Sacar");
+				
+				break;
+             case 7:
+				System.out.println("\n Depositar");
+				
+				break;
+             case 8:
+				System.out.println("\n Transferir");
+				
+				break;
+			default:
+				System.out.println("\nOpção Inválida");
+                 break;
+			}
+        }
 	}
-
 }
